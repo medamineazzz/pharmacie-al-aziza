ef395a681c546da458849f40066ac2b43f577cdb
medamineazzz <amineazzouzi16@gmail.com>
2026-01-12 17:30:15 +0100

    Add medicines.js with initial medicine list

---
+++ medicines.js
@@ -0,0 +1,6 @@
+const medicines = [
+  { name: "Paracétamol 500mg", price: 3.5 },
+  { name: "Ibuprofène 400mg", price: 5 },
+  { name: "Vitamine C 1000mg", price: 6 },
+  { name: "Amoxicilline 1g", price: 12 }
+];
