2035b96f5e860e2fb6947abb317c5e96f2aff4e3
medamineazzz <amineazzouzi16@gmail.com>
2026-01-12 18:09:34 +0100

    Fix decimal notation for medicine prices

--- medicines.js
+++ medicines.js
@@ -2,8 +2,8 @@ const medicines = [
   { name: "Paracétamol 500mg", price: 3.5 },
   { name: "Ibuprofène 400mg", price: 5 },
   { name: "Vitamine C 1000mg", price: 6 },
-  { name: "Doliprane 1000mg", price: 13,10 }
+  { name: "Doliprane 1000mg", price: 13.10 }
   { name: "Brufen", price: 30 }
-  { name: "Aximycine 1g/14", price: 59,90 }
+  { name: "Aximycine 1g/14", price: 59.90 }
   { name: "Nursie 1 GM", price: 169 }
 ];
